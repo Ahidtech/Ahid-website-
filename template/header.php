45b2d1b13c1ba12ee0af884b425c6136707edd9f
Valentine112 <valenny112@gmail.com>
2021-06-05 22:45:47 +0100

    Homepage by Valentine

---
+++ template/header.php
@@ -0,0 +1,24 @@
+<style>
+    header{
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 77%;
+        
+        padding-bottom: 2%;
+    }
+    .logo-cover img{
+        height: 50px;
+        width: 125px;
+    }
+</style>
+<header>
+    <section>
+        <div class="logo-cover">
+            <img src="logo/ahid (2).png" alt="">
+        </div>
+    </section>
+    <section>
+
+    </section>
+</header>
