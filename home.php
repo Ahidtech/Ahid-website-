acba5270683d023ab816604780e014ce683dcb7e
Valentine112 <valenny112@gmail.com>
2021-06-08 01:17:09 +0100

    Setup account page by Valentine

--- home.php
+++ home.php
@@ -5,9 +5,196 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/style/fonts.css">
+    <link rel="stylesheet" href="src/style/home.css">
     <title>AHID</title>
 </head>
 <body>
-    <?php include "template/header.php"; ?>
+    <div class="container">
+        <header>
+            <section>
+                <div>
+                    <img src="src/icon/message-4652.svg" alt="">
+                </div>
+                <div>
+                    <span>AHID</span>
+                </div>
+            </section>
+        </header>
+        <main>
+            <div>
+                <label>Follow your favourite brand</label>
+                <select name="" id="">
+                    <option value="Nearby">Nearby</option>
+                </select>
+                <br>
+                <br>
+            </div>
+            <section class="first-section">
+                <article>
+                    <div class="logo">
+                        <img src="src/image/LT_2952621_02_27.jpg" alt="">
+                    </div>
+                    <div class="details">
+                        <div class="first-info">
+                            <span>Addidas</span>
+                            <span>Follow</span>
+                        </div>
+                        <div class="second-info">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                        </div>
+                        <div class="third-info">
+                            <span>We sell shoes and all</span>
+                        </div><hr>
+                        <div class="fourth-info">
+                            <span>Distance</span><br>
+                            <span>2 kilometers away from you</span>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div class="logo">
+                        <img src="src/image/LT_FB_IMG_16191379360993074955921_04_24.jpg" alt="">
+                    </div>
+                    <div class="details">
+                        <div class="first-info">
+                            <span>NIKE</span>
+                            <span>Follow</span>
+                        </div>
+                        <div class="second-info">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star.svg" alt="">
+                        </div>
+                        <div class="third-info">
+                            <span>We sell sports outfit and regular shoes</span>
+                        </div><hr>
+                        <div class="fourth-info">
+                            <span>Distance</span><br>
+                            <span>4 kilometers away from you</span>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div class="logo">
+                        <img src="src/image/LT_B3D43167-166A-457E-8133-2CE34B2B60DD147421_03_01.jpeg" alt="">
+                    </div>
+                    <div class="details">
+                        <div class="first-info">
+                            <span>NIKE</span>
+                            <span>Follow</span>
+                        </div>
+                        <div class="second-info">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star.svg" alt="">
+                            <img src="src/icon/star.svg" alt="">
+                        </div>
+                        <div class="third-info">
+                            <span>We sell sports outfit and regular shoes</span>
+                        </div><hr>
+                        <div class="fourth-info">
+                            <span>Distance</span><br>
+                            <span>6 kilometers away from you</span>
+                        </div>
+                    </div>
+                </article>
+            </section>
+
+            <!-- SECOND PART HERE -->
+
+            <div class="describe-option">
+                <label>Follow brands around you</label>
+                <select name="" id="">
+                    <option value="Nearby">Nearby</option>
+                </select>
+                <br>
+                <br>
+            </div>
+            <section class="first-section">
+                <article>
+                    <div class="logo">
+                        <img src="src/image/LT_2952621_02_27.jpg" alt="">
+                    </div>
+                    <div class="details">
+                        <div class="first-info">
+                            <span>Addidas</span>
+                            <span>Follow</span>
+                        </div>
+                        <div class="second-info">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                        </div>
+                        <div class="third-info">
+                            <span>We sell shoes and all</span>
+                        </div><hr>
+                        <div class="fourth-info">
+                            <span>Distance</span><br>
+                            <span>2 kilometers away from you</span>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div class="logo">
+                        <img src="src/image/LT_FB_IMG_16191379360993074955921_04_24.jpg" alt="">
+                    </div>
+                    <div class="details">
+                        <div class="first-info">
+                            <span>NIKE</span>
+                            <span>Follow</span>
+                        </div>
+                        <div class="second-info">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star.svg" alt="">
+                        </div>
+                        <div class="third-info">
+                            <span>We sell sports outfit and regular shoes</span>
+                        </div><hr>
+                        <div class="fourth-info">
+                            <span>Distance</span><br>
+                            <span>4 kilometers away from you</span>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div class="logo">
+                        <img src="src/image/LT_B3D43167-166A-457E-8133-2CE34B2B60DD147421_03_01.jpeg" alt="">
+                    </div>
+                    <div class="details">
+                        <div class="first-info">
+                            <span>NIKE</span>
+                            <span>Follow</span>
+                        </div>
+                        <div class="second-info">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star (2).svg" alt="">
+                            <img src="src/icon/star.svg" alt="">
+                            <img src="src/icon/star.svg" alt="">
+                        </div>
+                        <div class="third-info">
+                            <span>We sell sports outfit and regular shoes</span>
+                        </div><hr>
+                        <div class="fourth-info">
+                            <span>Distance</span><br>
+                            <span>6 kilometers away from you</span>
+                        </div>
+                    </div>
+                </article>
+            </section>
+        </main>
+    </div>
 </body>
 </html>
