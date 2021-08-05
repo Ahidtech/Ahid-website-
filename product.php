e3f15d2ffed531e7720aaf74604dc6317bc621ea
Valentine112 <valenny112@gmail.com>
2021-08-06 00:44:01 +0100

    Signup

---
+++ product.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/style/product.css">
+    <title>product_name</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <main>
+                <section>
+                    <img src="src\icon\message-4652.svg" alt="" srcset="">
+                </section>
+
+                <section>
+                    <div>
+                        <img src="src/icon/star.svg" alt="" srcset="">
+                    </div>
+                    <div>
+                        <h3>NIKE</h3>
+                    </div>
+                </section>
+
+                <section>
+                    <a href="http://">Explore</a>
+                </section>
+            </main>
+        </header>
+    </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/style/signup.css">
+    <link rel="stylesheet" href="src/style/fonts.css">
+    <title>SIGNUP</title>
+</head>
+<body>
+    <div class="container">
+        <section>
+            <header>
+               <div>
+                   <img src="src/icon/message-4652.svg" alt="" srcset="">
+               </div>
+               <div>
+                   <h1>Signup</h1>
+               </div>
+               <div></div>
+            </header>
+        </section>
+        <section>
+            <form action="post">
+                <div>
+                    <input type="text" name="fullName" id="fullName" placeholder="Full name">
+                </div>
+                <div>
+                    <input type="email" name="email" id="email" placeholder="Email">
+                </div>
+                <div>
+                    <input type="text" name="Phone" id="number" placeholder="Phone number">
+                </div>
+               <div>
+                <input type="password" name="password" id="password" placeholder="Password">
+               </div>
+               <div>
+                   <button type="submit">Signup</button>
+               </div>
+               <div>
+                   <span>
+                       Already a member? <a href="http://">Login</a>
+                   </span>
+               </div>
+            </form>
+        </section>
+        <footer>
+            <div>
+                <span>By signing up means that you have agreed to our <br><a href="">Terms & Conditions</a></span>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
